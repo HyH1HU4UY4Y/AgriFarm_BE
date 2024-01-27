964733f231f3174222812b725d37c0d795bff37e
QAnh <nqanh312@gmail.com>
2024-01-27 19:28:52 +0700

    [Disease]UC 16,17 API done

---
+++ src/Infrastructure/Infrastructure.Disease/Context/DiseaseContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SharedApplication.Persistence;
+using SharedDomain.Entities.Diagnosis;
+
+namespace Infrastructure.Disease.Context
+{
+    public class DiseaseContext : BaseDbContext
+    {
+        public DiseaseContext()
+        {
+        }
+
+        public DiseaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<DiseaseInfo> DiseaseInfos { get; set; }
+        public DbSet<DiseaseDiagnosis> DiseaseDiagnoses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
