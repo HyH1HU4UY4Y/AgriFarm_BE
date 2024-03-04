4c08993a60e40325cef8eb02a5d8c039c8a852d3
Huy <khaihuy144@gmail.com>
2024-03-04 12:50:29 +0700

    update service registration

--- src/Services/Service.FarmRegistration/Commands/RegistFarmCommand.cs
+++ src/Services/Service.FarmRegistration/Commands/RegistFarmCommand.cs
@@ -5,6 +5,9 @@ using Service.FarmRegistry.DTOs;
 using SharedDomain.Entities.Subscribe;
 using SharedDomain.Repositories.Base;
 using SharedDomain.Exceptions;
+using SharedDomain.Entities.FarmComponents;
+using SharedDomain.Entities.Users;
+using SharedDomain.Defaults;
 
 namespace Service.FarmRegistry.Commands
 {
@@ -17,39 +20,65 @@ namespace Service.FarmRegistry.Commands
         
     public class RegistFarmCommandhandler: IRequestHandler<RegistFarmCommand, RegisterFormResponse>
     {
-        private readonly ISQLRepository<RegistrationContext, FarmRegistration> _resRepo;
-        private readonly ISQLRepository<RegistrationContext, PackageSolution> _solRepo;
+        private readonly ISQLRepository<RegistrationContext, FarmRegistration> _forms;
+        private readonly ISQLRepository<RegistrationContext, PackageSolution> _packages;
+        private readonly ISQLRepository<RegistrationContext, Site> _sites;
+        private readonly ISQLRepository<RegistrationContext, MinimalUserInfo> _users;
         private readonly IUnitOfWork<RegistrationContext> _unitOfWork;
         private readonly IMapper _mapper;
 
         public RegistFarmCommandhandler(IMapper mapper,
             IUnitOfWork<RegistrationContext> unitOfWork,
-            ISQLRepository<RegistrationContext, FarmRegistration> resRepo
+            ISQLRepository<RegistrationContext, FarmRegistration> forms
 ,
-            ISQLRepository<RegistrationContext, PackageSolution> solRepo)
+            ISQLRepository<RegistrationContext, PackageSolution> packages,
+            ISQLRepository<RegistrationContext, Site> sites,
+            ISQLRepository<RegistrationContext, MinimalUserInfo> users)
         {
             _mapper = mapper;
             _unitOfWork = unitOfWork;
-            _resRepo = resRepo;
-            _solRepo = solRepo;
+            _forms = forms;
+            _packages = packages;
+            _sites = sites;
+            _users = users;
         }
 
-        public Task<RegisterFormResponse> Handle(RegistFarmCommand request, CancellationToken cancellationToken)
+        public async Task<RegisterFormResponse> Handle(RegistFarmCommand request, CancellationToken cancellationToken)
         {
-            
-            var solution = _solRepo.GetOne(e => e.Id == request.SolutionId).Result;
-            if (solution == null) throw new Exception("Solution not exist!");
+            string msg = string.Empty;
 
-            var emailFarm = _resRepo.GetOne(e => e.Email == request.Email).Result;
-            if (emailFarm != null) throw new Exception("Email exist");
+            var solution = _packages.GetOne(e => e.Id == request.SolutionId).Result;
+            if (solution == null) throw new BadRequestException("Solution not exist.");
+
+            var site = _sites.GetOne(e => e.Name == request.SiteName).Result;
+            var user = _users.GetOne(e => e.UserName == request.Email).Result;
+            //var forms = await _forms.GetMany();
+            if (site?.Name == request.SiteName 
+                /*|| forms.Any(e=>e.SiteName == request.SiteName 
+                            && e.IsApprove == DecisonOption.Waiting)*/
+                ) msg += "Farm Name already exists.";
+
+            if (site?.SiteCode == request.SiteCode
+                /*|| forms.Any(e => e.SiteCode == request.SiteCode
+                            && e.IsApprove == DecisonOption.Waiting)*/
+                ) msg += " Farm Code already exists.";
+
+            if (user?.UserName == request.Email
+                /*|| forms.Any(e => e.Email == request.Email
+                            && e.IsApprove == DecisonOption.Waiting)*/
+                ) msg += " Email already exists.";
+
+            if (!string.IsNullOrEmpty(msg.Trim())) { 
+                throw new BadRequestException(msg.Trim());
+            }
+
             var entity = _mapper.Map<FarmRegistration>(request);
             entity.Cost = solution.Price;
-            _resRepo.AddAsync(entity);
+            await _forms.AddAsync(entity);
             
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-            _unitOfWork.SaveChangesAsync(cancellationToken);
-
-            return Task.FromResult(_mapper.Map<RegisterFormResponse>(entity));
+            return  _mapper.Map<RegisterFormResponse>(entity);
         }
     }
 }
---
+++ src/Services/Service.FarmRegistration/Consumers/Replication/FarmReplicatedConsumer.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using EventBus.Events.Messages;
+using Infrastructure.Common.Replication.Commands;
+using MassTransit;
+using MediatR;
+using SharedDomain.Entities.FarmComponents;
+
+namespace Service.Registration.Consumers.Replication
+{
+    public class FarmReplicatedConsumer : IConsumer<IntegrationEventMessage<Site>>
+    {
+        private IMapper _mapper;
+        private IMediator _mediator;
+        private ILogger<FarmReplicatedConsumer> _logger;
+
+        public FarmReplicatedConsumer(IMapper mapper,
+            IMediator mediator,
+            ILogger<FarmReplicatedConsumer> logger)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<IntegrationEventMessage<Site>> context)
+        {
+            var detail = context.Message.Data;
+
+
+            await _mediator.Send(new SaveEntityCommand<Site>
+            {
+                Item = detail,
+                State = context.Message.State
+            });
+        }
+    }
+}
